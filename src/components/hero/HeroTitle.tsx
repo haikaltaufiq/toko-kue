55b535cfb7929cafeed0cfb0aee960091e180a8c
HaikalTaufiq <134383454+HaikalTaufiq@users.noreply.github.com>
2026-08-07 20:01:20 +0700

    Edit : Test

--- src/components/hero/HeroTitle.tsx
+++ src/components/hero/HeroTitle.tsx
@@ -3,7 +3,7 @@
 export default function HeroTitle() {
   return (
     <h1 className="text-3xl md:text-4xl xl:text-5xl font-semibold tracking-tight leading-tight mb-2">
-      Toko Kue Lezat Bjir lah
+      Toko Kue Bawang
     </h1>
   );
 }
